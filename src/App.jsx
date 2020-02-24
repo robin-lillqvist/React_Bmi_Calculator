80197cc51f88b81abc6c0b88a374f49b33ab124d
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-24 11:08:51 +0100

    added tests for the form and added form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from "react";
+import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends Component {
+  state = {
+    weight: "",
+    height: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
