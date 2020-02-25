3e7e76633ee8d13cf3fefc8637c599521e3f1eff
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-25 13:24:35 +0100

    update compare equal amount

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,22 +2,20 @@ import React from "react";
 
 const Form = props => {
 
-  const weightPlaceholder = props.countingSystem == "metric" ? "Weight in kgs" : "Weight in Pounds"
-  const heightPlaceholder = props.countingSystem == "metric" ? "Height in cm" : "Height in Feet"
+  const weightPlaceholder = props.countingSystem === "metric" ? "Weight in kgs" : "Weight in Pounds"
+  const heightPlaceholder = props.countingSystem === "metric" ? "Height in cm" : "Height in Feet"
 
   return (
-    <form onSubmit={props.onSubmitHandler}>
-
-      <select name="countingSystem" id="select-method" 
+    <form className="formSubmit" onSubmit={props.onSubmitHandler}>
+      <select className="break_small" name="countingSystem" id="select-method" 
         onChange={props.onChangeHandler}
       >
         <option name="metric" value="metric">Metric</option>
         <option name="imperial" value="imperial">Imperial</option>
-
       </select>
-
       <label htmlFor="weight">Weight</label>
       <input
+        className="break"
         type="number"
         required
         placeholder={weightPlaceholder}
@@ -28,6 +26,7 @@ const Form = props => {
       />
       <label htmlFor="height">Height</label>
       <input
+        className="break"
         type="number"
         required
         placeholder={heightPlaceholder}
@@ -36,7 +35,7 @@ const Form = props => {
         id="height"
         onChange={props.onChangeHandler}
       />
-      <button id='calculate'>Calculate BMI</button>
+      <button className="break_small" id='calculate'>Calculate BMI</button>
     </form>
   );
 };
